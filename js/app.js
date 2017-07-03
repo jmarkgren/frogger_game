136ef1080399fd983722049523cbfec1d9d99727
Janelle Markgren <jsmarkgren@gmail.com>
2017-07-03 18:31:47 -0400

    rodrigo destroyed the game

--- js/app.js
+++ js/app.js
@@ -47,9 +47,10 @@ var Player = function(x, y){
 // a handleInput() method.
 Player.prototype.update = function(dt) {
 //When player gets to the water, you win!
-    if (this.y < -29) {
-        alert("You win!");
-        this.reset();
+    if (this.y < -30) {
+
+
+        //this.reset();
     }
 //Player can't move down off canvas
     if (this.y > 420) {
